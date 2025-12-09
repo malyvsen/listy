8cc5aca90cdce0b2d642ad517a31fcb73dc56683
malyvsen <5940672+malyvsen@users.noreply.github.com>
2025-12-09 23:48:46 +0100

    feat: make cli usage clearer to first-time users

--- listy/__main__.py
+++ listy/__main__.py
@@ -8,10 +8,15 @@ from .transcribe import transcribe
 
 @aclick.command()
 @aclick.argument(
-    "paths", type=aclick.Path(exists=True, dir_okay=False, path_type=Path), nargs=-1
+    "paths",
+    type=aclick.Path(exists=True, dir_okay=False, path_type=Path),
+    nargs=-1,
+    required=True,
 )
 @aclick.option("--language", default="pl")
 async def main(paths: tuple[Path, ...], language: str):
+    """Transcribe audio files, printing to the console."""
+
     transcriptions = [
         await transcribe(path, language) for path in tqdm(paths, desc="Transcribing")
     ]
